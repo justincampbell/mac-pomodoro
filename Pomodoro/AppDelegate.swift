abb1751cfa028967ddaab187e03891bf0ff73a31
Justin Campbell <justin@justincampbell.me>
2015-01-20 09:44:45 -0500

    Initial Commit

---
+++ Pomodoro/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Pomodoro
+//
+//  Created by Justin Campbell on 1/20/15.
+//  Copyright (c) 2015 Justin Campbell. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
